ea9b2f0fed39bbfc6687445254b2fdedfb430987
Deva <deva.samplemail@abc.com>
2021-10-05 16:32:04 +0530

    Alternate exporting of modules

---
+++ 6-alternative.js
@@ -0,0 +1,8 @@
+module.exports.items = ['item1', 'item2']
+const person = {
+    name: 'Deva',
+    age: 21,
+    company: 'TCS'
+}
+
+module.exports.employeeDeva = person;
---
+++ 7-exports.js
@@ -0,0 +1,5 @@
+const item = require('./6-alternative').items
+const deva = require('./6-alternative').employeeDeva
+
+console.log(deva);
+console.log(item);
